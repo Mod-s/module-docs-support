85bad9343d9e1a177ad38b2d6a9c8a21cfd6dbb9
Stacy Burris <stacy1burris@gmail.com>
2021-01-04 18:13:45 -0800

    update record console.log error

--- update/index.js
+++ update/index.js
@@ -4,29 +4,31 @@
 const moduleDocsModel = require('./moduleDocsModel.schema');
 
 exports.handler = async (event) => {
-  console.log('__EVENT__PUT:', event);
-  const { updatedMultipleUrl } = JSON.parse(event.body);
+  console.log('1.__EVENT__PUT:', event);
+  let { multipleUrl } = JSON.parse(event.body);
+
+  //const updateMultipleUrl = JSON.parse(event.body.multipleUrl);
 
   const id = event.pathParameters.id
 
-  console.log('__Updated MultipleURL From User__: ', updatedMultipleUrl);
+  console.log('2__Updated MultipleURL From User__: ', multipleUrl);
 
   try {
     let data;
 
     data = await moduleDocsModel.query('id').eq(id).limit(1).exec();
 
-    console.log('__Existing Record__: ', data);
+    console.log('3.__Existing Record__: ', data);
 
-    let name = data.name;
-    let mainUrl = data.mainUrl;
-    let multipleUrl = data.multipleUrl.push(updatedMultipleUrl);
-    let description = data.description;
-    let protect = data.protect;
+    let name = data.Document.name;
+    let mainUrl = data.Document.mainUrl;
+    multipleUrl = data.Document.multipleUrl.push(multipleUrl);
+    let description = data.Document.description;
+    let protect = data.Document.protect;
 
     const updatedData = await moduleDocsModel.update({ id, name, mainUrl, multipleUrl, description, protect });
 
-    console.log('__Updated Record__: ', update);
+    console.log('4.__Updated Record__: ', updateData);
 
     return {
       statusCode: 200,
