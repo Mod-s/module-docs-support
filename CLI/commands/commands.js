8f329ab50be8f4856d0fe8a246ce081962cd2ecf
Simon Panek <paneks19@gmail.com>
2021-01-06 17:36:55 -0800

    working on cli read command

--- CLI/commands/commands.js
+++ CLI/commands/commands.js
@@ -9,6 +9,7 @@ const superagent = require('superagent');
 const program = require('commander');
 const chalk = require('chalk');
 const inquirer = require('inquirer');
+const { description } = require('commander');
 //TODO: Does the dependency below need to be here or was it auto added?*************************************
 //const { listMods } = require('./prompts');
 program
@@ -22,7 +23,9 @@ program
   .alias('see') //TODO: can we change this to "l"? *********************************************************
   .description('Find the module to get started')
   .action(function () {
-    superagent.get('https://wmflq300d0.execute-api.us-west-2.amazonaws.com/module-docs-support/')
+    // superagent.get('https://wmflq300d0.execute-api.us-west-2.amazonaws.com/module-docs-support/') //obsolete: route prior to serverless deploy (save for now as backup)
+    superagent.get('https://ib9zg33bta.execute-api.us-west-2.amazonaws.com/modules/docs')
+    
 
       .then(response => {
         let info = JSON.parse(response.text);
@@ -38,35 +41,72 @@ program
         // })
 
         const question = [
-          { type: 'list', name: 'you chose', message: 'select a module', choices: list },
+          { type: 'list', name: 'you chose', message: 'Select a module', choices: list },
           // { type: 'list', name: 'doc', message: 'select a url', choices: url }
         ];
         // console.log('_____is info still info_______',info);
-        console.log('question after the question, but before the answer...', question)
+        // console.log('question after the question, but before the answer...', question)
 
         inquirer
         .prompt(question)
         .then(answer => {
-
-          console.log('we did not lose info', info);
+          // console.log('we did not lose info', info);
           //TO DO:
           //TURN RESPONSE INTO MODULE OBJECT
           //CHOICE TO OPEN(NAME AND URL) OR SEE MORE(POPS DESCRIPTION OR WHOLE OBJECT) 
-          console.log('what is the ansWer?', answer);
+          // console.log('what is the ansWer?', answer);
           let chosenModule = Object.values(answer);
-          console.log('I chose you....', chosenModule[0]);
+          let tempChoice;
+          // console.log('I chose you....', chosenModule[0]);
           info.forEach(item=>{
-            // console.log('can never have too many items:', item)
-            if(item.name === chosenModule[0]){
-              console.log(item)}
-
+            // console.log('can never have too many items:', item.name)
+            let matchCheck = item.name.toUpperCase();
+            if(matchCheck === chosenModule[0]){
+              tempChoice = item;
+              // console.log('this is the chosen object: ', item)}
+              // console.log(item.name.toUpperCase());
+              // console.log('Documentation URL: ', item.mainUrl);
+              // console.log('Description: ', item.description);
+            }
           })
+          showMainURL(tempChoice);
         })
+        
+        
       })
+      
+      // .catch(e => console.error('this is an error!', e))
+    })
+    
+function showMainURL (item){
+  console.log('Here is the item in the show function: ', item);
+  console.log('----------------------');
+  console.log('Module: ', chalk.bold.rgb(15, 125, 250)(item.name.toUpperCase()));
+  console.log('Description: ', chalk.bold.rgb(15, 125, 250)(item.description));
+  console.log('----------------------');
 
-    // .catch(e => console.error('this is an error!', e))
-  })
+  let mainUrlArray = [];
+  mainUrlArray.push(item.mainUrl);
 
+  let list = mainUrlArray.concat(item.multipleUrl);
+
+  const question2 = [
+    { type: 'list', name: 'you chose', message: 'Which doc would you like to open?', choices: list },
+  ];
+    inquirer
+    .prompt(question2)
+    .then(answer => {
+
+      let chosenUrl = Object.values(answer);
+      console.log('chosen url: ', chosenUrl);
+
+      //TODO: auto open the selected url
+      
+
+      console.log ('xdg-open www.google.com');
+      
+    })
+}
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 // SELECT SPECIFIC MODULE:::::::::::::::::::::::::::::::
@@ -146,7 +186,8 @@ program
     inquirer.prompt(addQuestions).then(
       function ({ name, description, url, multipleUrl = [], protect = false }) {
         //console.log('name: ', name, 'description: ', description, 'url: ', url);
-        superagent.post(`https://wmflq300d0.execute-api.us-west-2.amazonaws.com/module-docs-support`)
+        // superagent.post(`https://wmflq300d0.execute-api.us-west-2.amazonaws.com/module-docs-support`) //obsolete: from before serverless deploy (save for now as a back up)
+        superagent.post(`https://ib9zg33bta.execute-api.us-west-2.amazonaws.com/modules/docs`)
           .send({ "name": `${name}`, "description": `${description}`, "mainUrl": `${url}`, multipleUrl, protect })
           .then(response => {
             let info = JSON.parse(response.text);
@@ -196,7 +237,8 @@ program
   .alias('ad')
   .description('Add a URL to a module')
   .action(function (module, url) {
-    superagent.get('https://wmflq300d0.execute-api.us-west-2.amazonaws.com/module-docs-support/')
+    // superagent.get('https://wmflq300d0.execute-api.us-west-2.amazonaws.com/module-docs-support/') //obsolete: from before serverless deploy (save for now as backup)
+    superagent.get('https://ib9zg33bta.execute-api.us-west-2.amazonaws.com/modules/docs')
       .then(results => {
         let info = JSON.parse(results.text);
         let idToUpdate;
@@ -210,7 +252,9 @@ program
       .then(information => {
         console.log('INFORMATION', information.idToUpdate);
         let newURL = url.args[1];
-        superagent.put(`https://wmflq300d0.execute-api.us-west-2.amazonaws.com/module-docs-support/${information.id}`)
+        // superagent.put(`https://wmflq300d0.execute-api.us-west-2.amazonaws.com/module-docs-support/${information.id}`) //obsolete: from before serverless deploy (save for now as backup)
+        superagent.put(`https://ib9zg33bta.execute-api.us-west-2.amazonaws.com/modules/docs/${information.id}`)
+        
           .send({ "name": `${information.name}`, "description": `${information.description}`, "mainUrl": `${information.url}`, "multipleUrl": `${information.multipleUrl}`, "protect": `${information.protect}` })
           .then(res => {
             //TODO: Capture response and display the updated record
