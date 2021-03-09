58789626e4be9ba1a62a12d2864fa0063826de1f
Simon Panek <paneks19@gmail.com>
2021-03-09 14:44:43 -0800

    refactor the update function for mongoose

--- src/functions/delete.js
+++ src/functions/delete.js
@@ -1,57 +1,48 @@
 'use strict';
 
-
-
-const moduleDocsModel = require('../models/modules.schema.js');
+const Collection = require('../models/dataCollection.js');
+const mods = new Collection();
 
 //TODO: refactor to use req/res
 //TODO: refactor responses
 
-const deleteHandler = async (event) => {
+const deleteHandler = async (req, res) => {
 
-  console.log('DELETE deleteHandler event ', event);
+  console.log('DELETE deleteHandler req.body ', req.body);
 
-  console.log('1.__EVENT__PUT:', event);
-
-  const id = event.pathParameters.id; //capture record id from the user
-  const { deleteRecord } = JSON.parse(event.body); //user input requesting to delete record (users sends 1 to delete whole record or 0 to delete only a user url)
+  const id = req.params.id; //capture record id from the user
+  const { deleteRecord } = req.body; //user input requesting to delete record (users sends 1 to delete whole record or 0 to delete only a user url)
 
   // if (deleteRecord === 0) { //check to see if the user is trying to remove a user-created url
-  const { urlToDelete } = JSON.parse(event.body); //capture the index position of the user-generated url to delete
-  console.log('__URL Index To Delete From User__: ', urlToDelete);
+  const { urlToDelete } = req.body; //capture the index position of the user-generated url to delete
+  
   // }
 
   try {
     let data;
 
-    data = await moduleDocsModel.query('id').eq(id).limit(1).exec(); //check DB for existing record
-    let protect = data[0].protect; //set temp variable to hold the protect status of the record
+    data = await mods.get(id); //check DB for existing record
+    let protect = data.protect; //set temp variable to hold the protect status of the record
 
     console.log('__Existing Record__: ', data[0]);
 
     if (deleteRecord === 1 && protect === false) { //check if user wants to delete the whole record and the record is not protect
-      await moduleDocsModel.delete(id); //delete the whole record
-      return {
-        statusCode: 200,
-        body: 'Record deleted successfully.' //return an empty object
-      }
+      await mods.delete(id); //delete the whole record
+      res.status(200).send('Record deleted successfully');
     }
 
     if (deleteRecord === 1 && protect === true) { //check to see if user wants to delete the entire record and if it is protect
-      return {
-        statusCode: 403,
-        body: 'This record is protected. User may only delete user created content. If you would like to delete a user created URL, please try again, but do not attempt to delete the entire record.' //return a message indicating that deletion of a protect record is not allowed
+      res.status(403).send('This record is protected. User may only delete user created content. If you would like to delete a user created URL, please try again, but do not attempt to delete the entire record.'); //return a message indicating that deletion of a protected record is not allowed
       }
-    }
-
+    
     if (deleteRecord === 0) {
-      console.log('A. _______data[0]', data[0]);
-      let name = data[0].name; //set temp variables to hold existing data
-      let mainUrl = data[0].mainUrl;
+      console.log('A. _______data[0]', data);
+      let name = data.name; //set temp variables to hold existing data
+      let mainUrl = data.mainUrl;
 
       console.log('A.5. ______urlToDelete', urlToDelete);
 
-      data[0].multipleUrl.splice(urlToDelete, 1); //updated the user-created url array, remove the selected index position
+      data.multipleUrl.splice(urlToDelete, 1); //update the user-created url array, remove the selected index position
 
       let multipleUrl = data[0].multipleUrl;
 
@@ -61,27 +52,21 @@ const deleteHandler = async (event) => {
       //IF WE LET MULTIPLEURL = [], IT DELETES EVERYTHING IN IT
       //PROFIT
 
-      let description = data[0].description;
+      let description = data.description;
 
       // console.log('wow, what a let down', typeof(multipleUrl));
 
-      const updatedData = await moduleDocsModel.update({ id, name, mainUrl, multipleUrl, description, protect }); //update the record - only update should be in the multipleUrl
+      const updatedData = await mods.update({ id, name, mainUrl, multipleUrl, description, protect }); //update the record - only update should be in the multipleUrl
 
       console.log('__Updated Record__: ', updatedData);
 
-      return {
-        statusCode: 200,
-        body: JSON.stringify(updatedData) //return updated record
-      }
+      res.status(200).json(updatedData); //return updated record
+      
     }
 
   } catch (e) {
     console.log('-------------error message ----------', e)
-    return {
-
-      statusCode: 500,
-      response: e.message
-    }
+    res.status(500).send(e.message);
   }
 }
 
--- src/functions/update.js
+++ src/functions/update.js
@@ -4,54 +4,33 @@
 const Collection = require('../models/dataCollection.js');
 const mods = new Collection();
 
-//TODO: refactor to use req/res
-//TODO: refactor responses
-
 const updateHandler = async (req, res) => {
 
-console.log('UPDATE updateHandler req.body ', req.body);
+// console.log('UPDATE updateHandler req.body ', req.body);
 
-  // console.log('1.__EVENT__PUT:', event);
   let { updateUrl } = req.body;
-
-  //const updateMultipleUrl = JSON.parse(event.body.multipleUrl);
 
   const id = req.params.id
 
-  // console.log('2__Updated updateUrl From User__: ', updateUrl);
-  //new urls that were sent with put request (updateUrl) [ 'www.here9000.com', 'www.backagain.com' ]
   try {
     let data;
 
     data = await mods.get(id);
 
-console.log('UPDATE updateHandler data ', data);
-    // console.log('A.____ Data at 0: ', data[0]);
-    //document object {}
-
-    // console.log('B.____ Data[0].multipleUrl: ', data[0].multipleUrl);
-    //original url's to be updated [ 'www.here1.com', 'www.here2.com' ]
-
+// console.log('UPDATE updateHandler data ', data);
+    
     let name = data.name;
     let mainUrl = data.mainUrl;
-    // console.log('mainUrl after query:', mainUrl)
-    // let multipleUrl = data[0].multipleUrl.push(updateUrl); //
     let multipleUrl = data.multipleUrl.concat(updateUrl);
-    // let multipleUrl = updateUrl;
-    // console.log('C.____ this is a test of the emergency broadcast system...this is only a test');
     let description = data.description;
     let protect = data.protect;
-
     let dbID = data._id;
-    // console.log('D.____ multipleUrl updated: ', multipleUrl);
-
     let updatedObj = { name, mainUrl, multipleUrl, description, protect }
 
     const updatedData = await mods.update(dbID, updatedObj);
   
-console.log('UPDATE updateHandler updatedData ', updatedData);
+// console.log('UPDATE updateHandler updatedData ', updatedData);
 
-    // console.log('3.__Updated Record__: ', updatedData);
     res.status(200).json(updatedData);
 
   } catch (e) {
